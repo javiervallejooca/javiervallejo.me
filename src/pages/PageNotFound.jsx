e7577d849215f82c96bae8b4bb07d89ab061ec34
Javier Vallejo <javaoca@gmail.com>
2023-01-30 20:50:18 +0100

    Maquetación 404

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -6,7 +6,7 @@ const PageNotFound = () => {
       <div className="relative w-5/6 sm:w-3/4 md:w-2/5 lg:w-2/5 xl:w-1/3 xl:max-w-sm">
         <div className="overflow-hidden rounded py-5 text-left text-slate-700 dark:text-slate-300">
           <h1 className="mb-5 flex text-5xl font-black">Error 404</h1>
-          <p>
+          <p className="pb-4">
             La página 404 de toda la vida..., desconozco cómo has llegado hasta
             aquí ...
           </p>

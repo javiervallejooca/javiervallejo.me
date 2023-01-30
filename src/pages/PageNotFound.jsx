ee9ec14dd0f0f93086398bd4896dd2985d7479d4
Javier Vallejo <javaoca@gmail.com>
2023-01-30 20:47:24 +0100

    Mejoras página de error 404 #12

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,15 +1,16 @@
+import GoBack from "../components/about/GoBack";
+
 const PageNotFound = () => {
   return (
     <div className="flex h-screen items-center justify-center">
       <div className="relative w-5/6 sm:w-3/4 md:w-2/5 lg:w-2/5 xl:w-1/3 xl:max-w-sm">
-        <div className="overflow-hidden rounded bg-slate-100 py-5 text-center text-slate-700 shadow-md dark:bg-slate-900 dark:text-slate-300">
-          <h1 className="mb-5 flex text-center text-5xl font-black">
-            Error 404
-          </h1>
+        <div className="overflow-hidden rounded py-5 text-center text-slate-700 dark:text-slate-300">
+          <h1 className="mb-5 flex text-5xl font-black">Error 404</h1>
           <p>
             La página 404 de toda la vida..., desconozco cómo has llegado hasta
             aquí ...
           </p>
+          <GoBack />
         </div>
       </div>
     </div>

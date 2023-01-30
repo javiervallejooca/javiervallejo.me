fe7aecc89f7459dc8613dca949c503ea44a7b69f
Javier Vallejo <javaoca@gmail.com>
2023-01-30 23:04:02 +0100

    Componente de Loading #11

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
-import React, { Suspense, lazy } from "react";
+import { Suspense, lazy } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MyLayoutWithoutHeader from "./layouts/MyLayoutWithoutHeader";
 
 const About = lazy(() => import("./pages/About"));
 const Home = lazy(() => import("./pages/Home"));
 const PageNotFound = lazy(() => import("./pages/PageNotFound"));
+const Loading = lazy(() => import("./components/Loading"));
 
 function App() {
   return (
     <BrowserRouter>
-      <Suspense fallback={<div>Cargando...</div>}>
+      <Suspense fallback={<Loading />}>
         <Routes>
           <Route path="/" element={<MyLayoutWithoutHeader />}>
             <Route index element={<Home />}></Route>

67aba1f9d85077732762c6de009ff1b047db29f4
Javier Vallejo <javaoca@gmail.com>
2023-04-18 21:18:44 +0200

    Update skills

--- src/components/about/SummarySkills.jsx
+++ src/components/about/SummarySkills.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const SummarySkills = () => {
   return (
     <div className="relative mb-4 w-5/6 sm:w-3/4 md:w-3/5 xl:max-w-5xl">
-      <div className="sticky top-0 right-0 mb-4 bg-slate-100 py-4 px-4 text-xl font-bold text-slate-800 opacity-100 dark:bg-slate-900 dark:text-slate-200">
+      <div className="sticky right-0 top-0 mb-4 bg-slate-100 px-4 py-4 text-xl font-bold text-slate-800 opacity-100 dark:bg-slate-900 dark:text-slate-200">
         Resumen sobre mis habilidades o experiencia
       </div>
       <div className="text-slate-600 dark:text-slate-400">
@@ -24,6 +24,9 @@ const SummarySkills = () => {
           </li>
           <li>
             <b>React</b> ~ 1,5 años
+          </li>
+          <li>
+            <b>C#</b> ~ 1,5 años
           </li>
           <li>
             <b>WordPress</b> ~ 6 años

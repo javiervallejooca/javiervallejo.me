2d3923920d9a4b177817e4fc4857a116d7fe2d5b
Javier Vallejo <javaoca@gmail.com>
2024-01-23 18:15:01 +0100

    Upte vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+
+  build: {
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          chunk_react: ["react"],
+        },
+      },
+    },
+  },
+});

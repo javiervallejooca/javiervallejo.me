7e70555222297fce5e804e79f055686e4322ebee
Javier Vallejo <javaoca@gmail.com>
2025-02-14 17:20:48 +0100

    Added dark mode with tailwind v4

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -6,29 +6,29 @@ import Footer from "../components/shared/Footer";
 import LanguageSelector from "../components/shared/LanguageSelector";
 
 const MainLayout = () => {
-  // const [darkToggle, setDarkToggle] = useState(false);
+  const [darkToggle, setDarkToggle] = useState(false);
 
-  // useEffect(() => {
-  //   darkToggle
-  //     ? document.documentElement.classList.add("dark")
-  //     : document.documentElement.classList.remove("dark");
-  // }, [darkToggle]);
+  useEffect(() => {
+    darkToggle
+      ? document.documentElement.classList.add("dark")
+      : document.documentElement.classList.remove("dark");
+  }, [darkToggle]);
 
-  // useEffect(() => {
-  //   // Verificar si el navegador tiene modo oscuro
-  //   const darkModeMediaQuery = window.matchMedia(
-  //     "(prefers-color-scheme: dark)"
-  //   );
+  useEffect(() => {
+    // Verificar si el navegador tiene modo oscuro
+    const darkModeMediaQuery = window.matchMedia(
+      "(prefers-color-scheme: dark)"
+    );
 
-  //   // Actualizar el estado basado en el valor actual del media query
-  //   setDarkToggle(darkModeMediaQuery.matches);
-  // }, []);
+    // Actualizar el estado basado en el valor actual del media query
+    setDarkToggle(darkModeMediaQuery.matches);
+  }, []);
 
   return (
     <div className="w-full bg-gradient-to-b from-slate-200 to-slate-300 selection:bg-slate-600 selection:text-slate-100 dark:from-slate-800 dark:to-slate-900 dark:selection:bg-slate-200 dark:selection:text-slate-800">
       <main className="relative mb-0 pb-0">
         <LanguageSelector />
-        {/* <DarkMode darkToggle={darkToggle} setDarkToggle={setDarkToggle} /> */}
+        <DarkMode darkToggle={darkToggle} setDarkToggle={setDarkToggle} />
         <Outlet />
       </main>
       <Footer />
